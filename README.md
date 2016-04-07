c03a20a1fe7c7f502184cbcda51443bd9508cd02
Michael Smith <michael.smith@puppetlabs.com>
2016-04-06 21:03:09 -0700

    Add verify module method
    Adds a module method to verify whether a file is downloaded and
    complete.
    
    Also moves to using a single session for the module.

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 A simple Ruby BitTorrent client based on libtorrent-rasterbar
 
+## Setup
+
 Requires [Boost](https://boost.org) and [libtorrent](http://libtorrent.org).
+On Mac OS X these can be setup with Homebrew
+```
+brew install boost libtorrent-rasterbar
+```
+
+## Usage
+
+```
+# Returns true if the torrent payload is complete on-disk, false otherwise.
+TorrentClient.verify '<file>.torrent' '<destination_dir>'
+
+# Downloads the torrent payload to the destination_dir. Returns when complete.
+TorrentClient.download '<file>.torrent' '<destination_dir>'
+```
--- ext/torrent_client/torrent_client.cc
+++ ext/torrent_client/torrent_client.cc
@@ -5,33 +5,40 @@
 #include <libtorrent/torrent_info.hpp>
 #include <libtorrent/alert_types.hpp>
 
+namespace lt = libtorrent;
+
 VALUE TorrentClient = Qnil;
+std::auto_ptr<lt::session> _session;
 
 extern "C" {
     VALUE method_torrentclient_download(VALUE self, VALUE torrent_file, VALUE destination_dir);
+    VALUE method_torrentclient_verify(VALUE self, VALUE torrent_file, VALUE destination_dir);
 
     void Init_torrent_client()
     {
         TorrentClient = rb_define_module("TorrentClient");
         rb_define_singleton_method(TorrentClient, "download", RUBY_METHOD_FUNC(method_torrentclient_download), 2);
+        rb_define_singleton_method(TorrentClient, "verify", RUBY_METHOD_FUNC(method_torrentclient_verify), 2);
+
+        try {
+            _session.reset(new lt::session());
+            _session->set_alert_mask(lt::alert::status_notification | lt::alert::error_notification);
+
+            lt::error_code ec;
+            _session->listen_on(std::make_pair(6881, 6889), ec);
+            if (ec) {
+                std::string err = "failed to open listen socket: " + ec.message();
+                rb_raise(rb_eRuntimeError, err.c_str());
+            }
+        } catch (std::exception &e) {
+            rb_raise(rb_eRuntimeError, "failed from unknown exception");
+        }
     }
 
     VALUE method_torrentclient_download(VALUE self, VALUE torrent_file, VALUE destination_dir)
     {
         try {
-            namespace lt = libtorrent;
-
-            lt::session s;
-            s.set_alert_mask(lt::alert::status_notification | lt::alert::error_notification);
-
             lt::error_code ec;
-            s.listen_on(std::make_pair(6881, 6889), ec);
-            if (ec) {
-                std::string err = "failed to open listen socket: " + ec.message();
-                rb_raise(rb_eRuntimeError, err.c_str());
-                return Qnil;
-            }
-
             lt::add_torrent_params p;
             p.save_path = StringValueCStr(destination_dir);
             p.ti = new lt::torrent_info(StringValueCStr(torrent_file), ec);
@@ -40,19 +47,61 @@ extern "C" {
                 return Qnil;
             }
 
-            s.add_torrent(p, ec);
+            lt::torrent_handle handle = _session->add_torrent(p, ec);
             if (ec) {
                 rb_raise(rb_eRuntimeError, ec.message().c_str());
                 return Qnil;
             }
 
             while (true) {
-                if (s.wait_for_alert(lt::milliseconds(100))) {
-                    std::auto_ptr<lt::alert> a = s.pop_alert();
+                if (_session->wait_for_alert(lt::seconds(1))) {
+                    std::auto_ptr<lt::alert> a = _session->pop_alert();
                     if (a->type() == lt::torrent_finished_alert::alert_type) {
-                        return Qtrue;
+                        break;
                     }
                 }
+            }
+            _session->remove_torrent(handle);
+            return Qtrue;
+        } catch (std::exception &e) {
+            rb_raise(rb_eRuntimeError, "failed from unknown exception");
+        }
+        return Qnil;
+    }
+
+    VALUE method_torrentclient_verify(VALUE self, VALUE torrent_file, VALUE destination_dir)
+    {
+        try {
+            lt::error_code ec;
+            lt::add_torrent_params p;
+            p.save_path = StringValueCStr(destination_dir);
+            p.flags = lt::add_torrent_params::flag_paused;
+            p.ti = new lt::torrent_info(StringValueCStr(torrent_file), ec);
+            if (ec) {
+                rb_raise(rb_eRuntimeError, ec.message().c_str());
+                return Qnil;
+            }
+
+            lt::torrent_handle handle = _session->add_torrent(p, ec);
+            if (ec) {
+                rb_raise(rb_eRuntimeError, ec.message().c_str());
+                return Qnil;
+            }
+
+            lt::torrent_status status = handle.status();
+            while (status.state == lt::torrent_status::checking_files ||
+                   status.state == lt::torrent_status::checking_resume_data) {
+                _session->wait_for_alert(lt::seconds(1));
+                status = handle.status();
+            }
+
+            _session->remove_torrent(handle);
+
+            if (status.state == lt::torrent_status::seeding ||
+                status.state == lt::torrent_status::queued_for_checking) {
+                return Qtrue;
+            } else {
+                return Qfalse;
             }
         } catch (std::exception &e) {
             rb_raise(rb_eRuntimeError, "failed from unknown exception");
